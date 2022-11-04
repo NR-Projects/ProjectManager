e766e34255e3e524c7211648f53a69fc71b10688
Null-Root <triascend94@gmail.com>
2022-11-04 18:06:12 +0800

    Add: isValid function to check if operation is valid

--- src/repository/index.ts
+++ src/repository/index.ts
@@ -2,6 +2,29 @@ import { addProject, readAllProjects, updateProject, deleteProject } from './Pro
 import { addProjectTask, readAllProjectTasks, updateProjectTask, deleteProjectTask } from './ProjectTaskRepository';
 import { addTaskItem, readAllTaskItems, updateTaskItem, deleteTaskItem } from './TaskItemRepository';
 import requires from './IRequirement';
+import { BaseModel } from '@/models';
+
+function isValid(dataStatus: DataStatus, data: BaseModel, loadedDataList: Array<BaseModel>, newData?: BaseModel): boolean {
+    switch (dataStatus) {
+        case DataStatus.New:
+            break;
+        case DataStatus.Existing:
+            if (!newData) return false;
+            if (data.isEqual(newData)) return false;
+            break;
+    }
+
+    for (let item of loadedDataList) {
+        if (item.isEqual(data)) return false;
+    }
+
+    return true;
+}
+
+enum DataStatus {
+    New,
+    Existing
+}
 
 export {
     addProject,
@@ -19,5 +42,8 @@ export {
     updateTaskItem,
     deleteTaskItem,
 
-    requires
+    requires,
+
+    isValid,
+    DataStatus
 }
