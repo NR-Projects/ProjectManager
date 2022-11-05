794483bd065a9ceef32776034ce3cda554b9dd78
Null-Root <triascend94@gmail.com>
2022-11-05 19:56:39 +0800

    Add: modal functionality on adding projects

--- src/components/main_app/ProjectsArea.vue
+++ src/components/main_app/ProjectsArea.vue
@@ -2,40 +2,107 @@
     <div id="project-area">
         <div>
             <p>Your Projects:</p>
+            <div id="user-projects-tools">
+                <img @click="openModal" src="@/assets/img/add-item.svg" />
+                <img @click="loadProjects" src="@/assets/img/refresh.svg" />
+            </div>
             <div id="user-projects-container">
                 <ProjectItem
-                    v-for="sample_project in sampleProjects"
-                    :key="sample_project.id"
-                    :Data="sample_project" />
+                    v-for="project in projects"
+                    :key="project.id"
+                    :Data="project" />
             </div>
         </div>
     </div>
+
+    <Teleport to="#app">
+        <div ref="modal_ref" class="modal">
+            <div class="modal-content">
+                <p class="modal-title">This is a modal</p>
+                <div class="modal-inputs">
+                    <div class="modal-input">
+                        <label>Project Name:</label>
+                        <input type="text" />
+                    </div>
+                    <div class="modal-input">
+                        <label>Project Description:</label>
+                        <input type="text" />
+                    </div>
+                </div>
+                <div class="modal-options">
+                    <div @click="closeModal">
+                        <span>Close</span>
+                    </div>
+                    <div @click="submitAddProject">
+                        <span>Submit</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </Teleport>
 </template>
 
 <script lang="ts">
-    import { defineComponent } from 'vue';
+    import { defineComponent, ref } from 'vue';
     import ProjectItem from '@/components/main_app/ProjectItem.vue';
-    import { Project } from '@/models/';
+    import { Project } from '@/models';
+    import { addProject, readAllProjects, isValid, DataStatus } from '@/repository';
+    import { toggleModal } from '@/utilities';
 
     export default defineComponent({
         name: 'ProjectsArea',
         components: {
             ProjectItem
         },
-        data () {
+        setup() {
+            // Elements
+            const modal_ref = ref(null);
+
+            // Variables
+            const projects = ref(Array<Project>());
+            const add_project_name = ref('');
+            const add_project_desc = ref('');
+
+            const loadProjects = () => {
+                readAllProjects().then((value) => {
+                    projects.value = value;
+                })
+            };
+
+            const submitAddProject = () => {
+                // Get and process data
+                const newProject: Project = new Project(add_project_name.value, add_project_desc.value);
+
+                if ( !isValid(DataStatus.New, newProject, projects.value) ) return;
+
+                // Send to firebase
+                addProject(newProject);
+
+                // Close modal
+                toggleModal('close', modal_ref.value as unknown as HTMLElement);
+            };
+
+            const openModal = () => {
+                // Open modal
+                toggleModal('open', modal_ref.value as unknown as HTMLElement);
+            };
+
+            const closeModal = () => {
+                // Close modal
+                toggleModal('close', modal_ref.value as unknown as HTMLElement);
+            };
+
             return {
-                sampleProjects : [
-                    new Project('1', 'Sample 1', new Date(2020, 2, 23), new Date()),
-                    new Project('2', 'Sample 2', new Date(2021, 5, 24), new Date()),
-                    new Project('3', 'Sample 3', new Date(2022, 11, 18), new Date()),
-                    new Project('4', 'Sample 4', new Date(2022, 10, 3), new Date()),
-                ]
-            }
+                projects,
+                submitAddProject, loadProjects,
+                openModal, closeModal,
+                modal_ref
+            };
+        },
+        mounted() {
+            this.loadProjects();
         }
     });
-
-
-
 </script>
 
 <style lang="scss" scoped>
@@ -54,6 +121,22 @@
                 text-align: left;
                 color: white;
                 font-size: 1.5rem;
+            }
+        }
+    }
+
+    #user-projects-tools {
+        display: flex;
+        align-items: center;
+        justify-content: right;
+        padding-right: 5vw;
+
+        & > img {
+            width: 30px;
+            margin: 0.5vw;
+
+            &:hover {
+                cursor:pointer;
             }
         }
     }
