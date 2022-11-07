fa1e6b078270da506e8b5ea2375d125c97b65c7a
Null-Root <triascend94@gmail.com>
2022-11-07 15:20:41 +0800

    Add: creation and check for null objects

--- src/models/BaseModel.ts
+++ src/models/BaseModel.ts
@@ -1,11 +1,23 @@
 export default class BaseModel {
     name: string;
+    isEmpty: boolean
 
-    constructor(_name: string) {
+    constructor(_name: string, isModelNull?: boolean) {
         this.name = _name;
+        this.isEmpty = false;
+
+        if ( isModelNull ) this.isEmpty = isModelNull;
+    }
+
+    static createEmptyObject(): BaseModel {
+        return new BaseModel("", true);
     }
 
     isEqual(other: BaseModel): boolean {
         return this.name === other.name;
+    }
+
+    isNull(): boolean {
+        return this.isEmpty;
     }
 }
