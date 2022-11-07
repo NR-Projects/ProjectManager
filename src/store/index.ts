251cfe0391f2cd45e0f7867d21da5b55ba2f1730
Null-Root <triascend94@gmail.com>
2022-11-07 15:21:43 +0800

    Add: current project loaded in vuex store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,25 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import { Project } from '@/models';
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+	state: {
+		projectLoaded: Project.createEmptyObject()
+	},
+	getters: {
+		getprojectLoaded(state) {
+			return state.projectLoaded;
+		}
+	},
+	mutations: {
+		SET_PROJECT_LOADED(state, newProject: Project) {
+			state.projectLoaded = newProject;
+		}
+	},
+	actions: {
+		setProjectLoaded({ commit }, newProject: Project) {
+			commit('SET_PROJECT_LOADED', newProject);
+		}
+	},
+	modules: {
+	}
 })
