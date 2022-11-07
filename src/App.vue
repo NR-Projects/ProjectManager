1365af81e6f404b3c4dbba85a4a758e701a384c5
Null-Root <triascend94@gmail.com>
2022-11-07 21:41:14 +0800

    Add: Custom scrollbar style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-	<router-view/>
+	<router-view />
 </template>
 
 <style lang="scss">
@@ -13,12 +13,38 @@
 	height: 100%;
 }
 
-html, body {
+html,
+body {
 	margin: 0;
 	padding: 0;
 	box-sizing: border-box;
 	display: block;
 	width: 100vw;
 	height: 100%;
+}
+
+// Modified from w3schools.com and https://codepen.io/devstreak/pen/dMYgeO
+/* width */
+::-webkit-scrollbar {
+	width: 10px;
+	height: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+	background: #f1f1f1;
+	-webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+	box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+	background: #888;
+	border: 2px solid #555555;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+	background: #555;
 }
 </style>
