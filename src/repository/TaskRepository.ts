c880316e247666cdf6c67252130208775b48bd0a
Null-Root <triascend94@gmail.com>
2022-11-08 21:01:39 +0800

    Add: implementation on addTask function

--- src/repository/TaskRepository.ts
+++ src/repository/TaskRepository.ts
@@ -1,12 +1,38 @@
 import { requires } from './IRequirement';
+import { getAuth } from 'firebase/auth';
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    getDocs,
+    updateDoc,
+    deleteDoc,
+    query,
+    where, 
+    serverTimestamp} from "firebase/firestore";
 import { Task } from '@/models';
 
-function addTask(require: requires) {
-    //
+async function addTask(require: requires, task: Task): Promise<void> {
+    const { name, desc, status } = task;
+
+    if( !require ) return;
+    const project_id = require.projectId!;
+    const projectTask_id = require.projectTaskId!;
+
+    const uid = getAuth().currentUser?.uid;
+    if ( !uid ) return;
+
+    await addDoc(collection(getFirestore(), "users", uid, "projects", project_id, "project_task", projectTask_id, "task"), {
+        name: name,
+        desc: desc,
+        status: status,
+        date_created: serverTimestamp(),
+        date_last_updated: serverTimestamp()
+    });
 }
 
-function readAllTasks(require: requires) {
-    //
+async function readAllTasks(require: requires): Promise<Array<Task>> {
+    return Array<Task>();
 }
 
 function updateTask(require: requires) {
